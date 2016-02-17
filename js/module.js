fc3f01862f95a0a4242e3939893a3e3ded0e68e7
Gurjot Singh <celica09@gmail.com>
2016-02-17 01:07:16 -0800

    third  commit

--- js/module.js
+++ js/module.js
@@ -6,12 +6,12 @@ app.constant('fbUrl', 'https://authmetes.firebaseio.com/')
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', {url: '/', templateUrl: '/html/home.html', controller: 'messageCtrl'})
+    .state('home', {url: '/', templateUrl: 'html/home.html', controller: 'messageCtrl'})
     .state('user', {url: '/user', template: '<ui-view/>', abstract: true})
-    .state('user.login', {url: '/login', templateUrl: '/html/user.html', controller: 'userCtrl'})
-    .state('user.register', {url: '/register', templateUrl: '/html/user.html', controller: 'userCtrl'})
+    .state('user.login', {url: '/login', templateUrl: 'html/user.html', controller: 'userCtrl'})
+    .state('user.register', {url: '/register', templateUrl: 'html/user.html', controller: 'userCtrl'})
 
-    .state('user.profile', {url: '/profile', templateUrl: '/html/profile.html', controller: 'profileCtrl',
+    .state('user.profile', {url: '/profile', templateUrl: 'html/profile.html', controller: 'profileCtrl',
       onEnter: function($state, fbAuth) {
         if (!fbAuth.$getAuth()) {
           $state.go('home'); 
